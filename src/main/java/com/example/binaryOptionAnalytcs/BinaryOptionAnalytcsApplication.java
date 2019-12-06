f8fc9a052e226af06f41811c9e5f81c11f1315c5
Marley Diniz <marley.diniz@gmail.com>
2019-12-06 11:45:19 -0300

    Projeto Criado

---
+++ src/main/java/com/example/binaryOptionAnalytcs/BinaryOptionAnalytcsApplication.java
@@ -0,0 +1,13 @@
+package com.example.binaryOptionAnalytcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinaryOptionAnalytcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinaryOptionAnalytcsApplication.class, args);
+	}
+
+}
